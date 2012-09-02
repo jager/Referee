8b9e7540fafc8ef0cade80097e8437ef38643023
Michał Jagusiak <michaljagusiak@gmail.com>
2012-09-02 22:57:04 +0200

    pusz na prod

---
+++ Referee/Helpers/HelperRoles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Referee.Helpers
+{
+    public static class HelperRoles
+    {
+        public const string Administrator = "Administrator";
+        public const string RefereatObsad = "RO";
+        public const string WydzialGieriEwidencji = "WGiE";
+        public const string Sedzia = "Sędzia";
+    }
+}
