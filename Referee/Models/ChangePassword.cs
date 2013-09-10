15dd21360cc5d717cb2802605fd07570d437acb3
Michał Jagusiak <michaljagusiak@gmail.com>
2013-09-10 22:38:39 +0200

    Zmiany do change password action

---
+++ Referee/Models/ChangePassword.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+
+namespace Referee.Models
+{
+    public class ChangePassword
+    {
+
+        private int _valid = 5;
+
+        [Key]
+        public string Token { get; set; }
+        public string RefereeId { get; set; }
+        public DateTime Added { get; set; }
+        public DateTime Updated { get; set; }
+
+        [NotMapped]
+        public int ValidPeriod
+        {
+            get
+            {
+                return _valid;
+            }
+        }
+
+        /// <summary>
+        /// Generates hash string.
+        /// </summary>
+        /// <returns>string</returns>
+        public string GenKey()
+        {
+            byte[] data = new byte[100];
+            byte[] result;
+            using (SHA256 shaM = new SHA256Managed())
+            {
+                result = shaM.ComputeHash(data);
+            }
+            return System.Text.UTF8Encoding.UTF8.GetString(result);
+        }
+
+        /// <summary>
+        /// Chcecks whether token is still valid
+        /// </summary>
+        /// <returns>bool</returns>
+        public bool isValid()
+        {
+            if (this.Added != null)
+            {
+                return (DateTime.Now <= this.Added.AddHours(this.ValidPeriod));
+            }
+            return false;
+        }
+    }
+}
---
+++ Referee/Repositories/ChangePasswordRepository.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebVolleyballManager.Models;
+
+
+namespace WebVolleyballManager.Repositories
+{
+    public class ChangePasswordRepository : GenericRepository<ChangePassword>
+    {
+
+        public ChangePasswordRepository(ManagerContext context) : base(context)
+        {            
+        }
+
+
+
+        /// <summary>
+        /// Creates and saves new ChangePasswod object 
+        /// </summary>
+        /// <param name="UserId"></param>
+        public void Create(string UserId)
+        {
+            ChangePassword Password = new ChangePassword();
+            Password.Added = DateTime.Now;
+            Password.Token = Password.GenKey();
+            Password.UserId = UserId;
+            this.Insert(Password);
+        }
+
+
+        /// <summary>
+        /// Returns ChangePassword object for given token
+        /// </summary>
+        /// <param name="Token"></param>
+        /// <returns></returns>
+        private ChangePassword GetToken(string Token)
+        {
+            return this.Get(filter: p => p.Token == Token)
+                       .SingleOrDefault<ChangePassword>();
+        }
+
+        /// <summary>
+        /// Checks wheather token is valid
+        /// </summary>
+        /// <param name="Token"></param>
+        /// <returns></returns>
+        public bool Check(string Token)
+        {
+            ChangePassword Password = this.GetToken(Token);
+            return Password.isValid();
+        }
+
+        /// <summary>
+        /// Changes Token
+        /// </summary>
+        /// <param name="Token"></param>
+        public void Change(string Token)
+        {
+            ChangePassword Password = this.GetToken(Token);
+            Password.Token = Password.GenKey();
+            Password.Updated = DateTime.Now;
+            this.Update(Password);
+        }
+    }
+}
