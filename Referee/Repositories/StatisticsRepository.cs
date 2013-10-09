aff5d7d1f35aeed507bef410de018c96021f395d
Michał Jagusiak <michaljagusiak@gmail.com>
2013-10-09 13:12:44 +0200

    Dodawanie niedyspozycyjnosci, uprawnienia dla sedziego, usuwanie jak jest w przyszlosci, dla RO zawsze

---
+++ Referee/Repositories/StatisticsRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Referee.Repositories
+{
+    public class StatisticsRepository
+    {
+    }
+}
