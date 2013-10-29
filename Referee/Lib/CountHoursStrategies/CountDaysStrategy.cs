77e0d0d66dc6f198f2fe55a3309f8346c6b52414
michaljagusiak@gmail.com <michaljagusiak@gmail.com>
2013-10-29 14:58:24 +0100

    Mechanizm zliczania dni niedyspozycyjnosci

---
+++ Referee/Lib/CountHoursStrategies/CountDaysStrategy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Referee.Lib.CountHoursStrategies
+{
+    public class CountDaysStrategy : ICountHours
+    {
+        private DateTime _startDate;
+        private DateTime _endDate;
+        private int _count = 0;
+        public CountDaysStrategy(DateTime start, DateTime end)
+        {
+            this._startDate = start;
+            this._endDate = end;
+        }
+
+        public int Count()
+        {
+            _count = this._endDate.Subtract(this._startDate).Days;
+            return _count;
+        }
+
+    }
+}
---
+++ Referee/Lib/CountHoursStrategies/WeekendCountStrategy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Referee.Lib.CountHoursStrategies
+{
+    public class WeekendCountStrategy : ICountHours
+    {
+        private DateTime _startDate;
+        private DateTime _endDate;
+        protected int Min = 8;
+        protected int Max = 22;
+        private int _count = 0;
+
+        public WeekendCountStrategy(DateTime start, DateTime end)
+        {
+            this._startDate = start;
+            this._endDate = end;
+        }
+
+        public int Count()
+        {
+            if (_startDate.Day == _endDate.Day)
+            {
+                int min = _startDate.Hour > Min ? _startDate.Hour : Min;
+                int max = _endDate.Hour < Max ? _endDate.Hour : Max;
+                _count = max - min;
+            }
+            return _count;
+        }
+    }
+}
